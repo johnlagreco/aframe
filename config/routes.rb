147536f2e95269e32d4b41f0293b391f2ced85a8
John LaGreco <johnlagreco@gmail.com>
2016-02-29 15:57:34 -0500

    Added model, set routes, added gems & migrated table.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'incoming/' => 'notifications#incoming'
+  post 'new/' => 'notifications#new'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'notifications#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
